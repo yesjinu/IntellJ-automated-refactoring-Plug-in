31dce4023b97d72aca8e045b22c756e9175c0a30
kmintae <shsesther928@postech.ac.kr>
2020-11-08 17:51:19 +0900

    #2. $2. InlineMethod.java added

---
+++ src/main/java/Model/Refactoring/ComposingMethods/InlineMethod.java
@@ -0,0 +1,4 @@
+package Model.Refactoring.ComposingMethods;
+
+public class InlineMethod {
+}
--- src/test/java/Tests/UnitTest/ComposingMethods/ComposingMethodsTest.java
+++ src/test/java/Tests/UnitTest/ComposingMethods/ComposingMethodsTest.java
@@ -15,4 +15,9 @@ public class ComposingMethodsTest {
     {
 
     }
+
+    @Test
+    public void ExtractVariableTest() {
+
+    }
 }
