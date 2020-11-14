9e5d507864f2a5697d4ccc4799721dc5187df015
kmintae <shsesther928@postech.ac.kr>
2020-11-14 23:15:10 +0900

    #5. $3. refactor!: packaging Really Finished (Test Name Changed)

--- src/main/java/ui/ui.java
+++ src/main/java/ui/ui.java
@@ -1,4 +1,5 @@
 package ui;
 
 public class ui {
+    // TODO: Implement
 }
--- src/test/java/tests/InlineMethodTest.java
+++ src/test/java/tests/InlineMethodTest.java
@@ -4,7 +4,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.junit.jupiter.api.Test;
 
-public class ComposingMethodsTest extends LightJavaCodeInsightFixtureTestCase {
+public class InlineMethodTest extends LightJavaCodeInsightFixtureTestCase {
 
     @Test
     public void fetchCandidateTest() {
