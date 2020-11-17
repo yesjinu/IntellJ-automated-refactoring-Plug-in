e1f68d698e6c9d091c0cf6f7c99c9c8bc4f7e0e8
seha park <rachesh@postech.ac.kr>
2020-11-17 23:37:48 +0900

    merge test

---
+++ src/main/java/refactoring/ExtractVariable.java
@@ -0,0 +1,10 @@
+package refactoring;
+
+import model.refactoring.RefactoringAlgorithm;
+
+public class ExtractVariable implements RefactoringAlgorithm {
+
+    public boolean isRefactorable() {
+        return false;
+    }
+}
