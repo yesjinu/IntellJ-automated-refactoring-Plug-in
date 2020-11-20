d6c79238fdd7768be742c6753d22e9686529538d
kmintae <shsesther928@postech.ac.kr>
2020-11-21 02:02:29 +0900

    #5. $14. fix!: Inline Method Restoring

--- src/main/java/wanted/refactoring/ExtractVariable.java
+++ src/main/java/wanted/refactoring/ExtractVariable.java
@@ -1,8 +1,25 @@
 package wanted.refactoring;
 
-public class ExtractVariable implements RefactoringAlgorithm {
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ExtractVariable extends BaseRefactorAction {
 
     public boolean isRefactorable() {
         return false;
+    }
+
+    @Override
+    public String storyName() {
+        return null;
+    }
+
+    @Override
+    public boolean refactorValid(AnActionEvent e) {
+        return false;
+    }
+
+    @Override
+    protected void refactor(AnActionEvent e) {
+
     }
 }
--- src/main/java/wanted/utils/FindPsi.java
+++ src/main/java/wanted/utils/FindPsi.java
@@ -83,4 +83,18 @@ public class FindPsi {
         });
         return ret;
     }
+
+    /**
+     * Searching for every subclasses
+     * @param superclass Superclass
+     * @param classList List of all classes in project
+     * @return List of all subclasses extends superclass
+     */
+    public static List<PsiClass> findEverySubClass (PsiClass superclass, List<PsiClass> classList) {
+        List<PsiClass> subclassList = new ArrayList<>();
+        for (PsiClass psiClass : classList)
+            if (Arrays.asList(psiClass.getSupers()).contains(superclass))
+                subclassList.add(psiClass);
+        return subclassList;
+    }
 }
