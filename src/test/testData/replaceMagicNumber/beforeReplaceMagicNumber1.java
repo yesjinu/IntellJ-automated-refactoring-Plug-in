e6187a2518dfa86f5821f0d80be41b99affb8bac
seha park <rachesh@postech.ac.kr>
2020-12-02 20:32:27 +0900

    [S-10] fix: restore content of test1
    remove useless line

--- src/test/testData/replaceMagicNumber/beforeReplaceMagicNumber1.java
+++ src/test/testData/replaceMagicNumber/beforeReplaceMagicNumber1.java
@@ -2,6 +2,4 @@ public class basic {
     double area(double radius) { return radius * radius * 3.141<caret>592; }
 
     double circumference(double radius) { return 2 * 3.141592 * radius;}
-
-    void method1(){ int x = <caret>3; short y = 3; byte z = 3; char w = 3; long k = 3;}
 }
