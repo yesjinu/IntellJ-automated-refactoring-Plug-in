72e34fbb2817690d79827b99c370ddb25e024b03
yesjinu <yes.jinu@gmail.com>
2020-12-05 00:53:52 +0900

    [TEST] Implement ReplacePsi::changeModifier
    Implement test codes by making instant class and method with string.

--- src/test/java/wanted/test/Utils/ReplacePsiTest.java
+++ src/test/java/wanted/test/Utils/ReplacePsiTest.java
@@ -1,9 +1,12 @@
 package wanted.test.Utils;
 
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiField;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
 import wanted.test.base.LightActionTestCase;
+import wanted.utils.AddPsi;
 import wanted.utils.ReplacePsi;
 
 import java.util.ArrayList;
@@ -44,5 +47,39 @@ public class ReplacePsiTest extends LightActionTestCase {
 
         testRunner.testMethod = ReplacePsi.class.getMethod("changeModifier", params);
         doTest();
+    }
+
+    public void testReplacePsi4inString() {
+        Project project = getProject();
+        PsiElementFactory factory = PsiElementFactory.getInstance(project);
+
+        // Create dummy class
+        // public class Temp {
+        //     public int value;
+        // }
+        final PsiClass targetClass = factory.createClass("Temp");
+        PsiField field = factory.createFieldFromText("public int value;", null);
+        PsiField newField = factory.createFieldFromText("private int value;", null);
+        targetClass.addAfter(field, targetClass.getLBrace());
+
+        System.out.println(targetClass.getText());
+
+        PsiField targetField = null;
+        for (PsiElement e : targetClass.getChildren()) {
+            if (e instanceof PsiField) {
+                targetField = (PsiField) e;
+                break;
+            }
+        }
+
+        List<String> add = new ArrayList<>(); add.add("private");
+        List<String> del = new ArrayList<>(); del.add("public");
+
+        PsiField finalTargetField = targetField;
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            ReplacePsi.changeModifier(finalTargetField, del, add);
+        });
+
+        assertEquals(finalTargetField.getText(), newField.getText());
     }
 }
