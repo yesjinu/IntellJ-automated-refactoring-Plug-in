d2978b6b82f254cb6701aa1c861f6ee0b57c6737
yesjinu <yes.jinu@gmail.com>
2020-12-06 03:52:55 +0900

    [TEST] Add test case in AddPsiTest and Write docs

--- src/test/java/wanted/test/Utils/AddPsiTest.java
+++ src/test/java/wanted/test/Utils/AddPsiTest.java
@@ -29,6 +29,8 @@ public class AddPsiTest extends LightActionTestCase {
         return super.getBasePath() + "/PsiUtilsTest/AddPsiTest";
     }
 
+    // test AddPsi::addMethod
+    // case 1 : Add one method to a class
     public void testAddMethod() {
         Project project = getProject();
         PsiElementFactory factory = PsiElementFactory.getInstance(project);
@@ -37,23 +39,24 @@ public class AddPsiTest extends LightActionTestCase {
         final PsiClass targetClass = factory.createClass("Temp");
 
         List<PsiElement> methodList = new ArrayList<>();
-        PsiMethod method = factory.createMethodFromText("void sendReport() { }", null);
+        PsiMethod method = factory.createMethodFromText("void tempMethod() { }", null);
         methodList.add(method);
-
 
         WriteCommandAction.runWriteCommandAction(project, () -> {
             AddPsi.addMethod(targetClass, methodList);
         });
 
-        assertFalse(targetClass.findMethodsByName("sendReport", false).length == 2);
-        assertTrue(targetClass.findMethodsByName("sendReport", false).length == 1);
-        assertFalse(targetClass.findMethodsByName("sendReport", false).length == 0);
+        assertFalse(targetClass.findMethodsByName("tempMethod", false).length == 2);
+        assertEquals(1, targetClass.findMethodsByName("tempMethod", false).length);
+        assertFalse(targetClass.findMethodsByName("tempMethod", false).length == 0);
 
         assertFalse(targetClass.findMethodsByName("anotherName", false).length == 2);
         assertFalse(targetClass.findMethodsByName("anotherName", false).length == 1);
-        assertTrue(targetClass.findMethodsByName("anotherName", false).length == 0);
+        assertEquals(0, targetClass.findMethodsByName("anotherName", false).length);
     }
 
+    // test AddPsi::addMethod
+    // case 2 : Add multiple methods to a class
     public void testAddMethod2() {
         Project project = getProject();
         PsiElementFactory factory = PsiElementFactory.getInstance(project);
@@ -61,8 +64,10 @@ public class AddPsiTest extends LightActionTestCase {
         final PsiClass targetClass = factory.createClass("Temp");
 
         List<PsiElement> methodList = new ArrayList<>();
-        PsiMethod method1 = factory.createMethodFromText("void sendReport1() { }", null);
-        PsiMethod method2 = factory.createMethodFromText("void sendReport2() { }", null);
+
+        PsiMethod method1 = factory.createMethodFromText("void tempMethod1() { }", null);
+        PsiMethod method2 = factory.createMethodFromText("void tempMethod2() { }", null);
+
         methodList.add(method1);
         methodList.add(method2);
 
@@ -71,10 +76,12 @@ public class AddPsiTest extends LightActionTestCase {
             AddPsi.addMethod(targetClass, methodList);
         });
 
-        assertTrue(targetClass.findMethodsByName("sendReport1", false).length == 1);
-        assertTrue(targetClass.findMethodsByName("sendReport2", false).length == 1);
+        assertTrue(targetClass.findMethodsByName("tempMethod1", false).length == 1);
+        assertTrue(targetClass.findMethodsByName("tempMethod2", false).length == 1);
     }
 
+    // test AddPsi::addField
+    // case 1 : Add two fields to an empty class
     public void testAddField() {
         Project project = getProject();
         PsiElementFactory factory = PsiElementFactory.getInstance(project);
@@ -89,6 +96,38 @@ public class AddPsiTest extends LightActionTestCase {
 
         String expected =
                 "public class Temp {int a = 0;boolean b = true; }";
+
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            AddPsi.addField(targetClass, fieldList);
+        });
+
+        Assertions.assertEquals(expected, targetClass.getText());
+
+    }
+
+    // test AddPsi::addField
+    // case 2 : Add two fields to an filled class
+    // addFields method should add fields at the end of the Psifield that already exists.
+    public void testAddField2() {
+        Project project = getProject();
+        PsiElementFactory factory = PsiElementFactory.getInstance(project);
+
+        final PsiClass targetClass = factory.createClass("Temp");
+        PsiMethod preMethod = factory.createMethodFromText("void doNothing() {}", null);
+        PsiField preField = factory.createFieldFromText("char c = 'c';", null);
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            targetClass.add(preMethod);
+            targetClass.add(preField);
+        });
+
+        List<PsiField> fieldList = new ArrayList<>();
+        PsiField field1 = factory.createFieldFromText("int a = 0;", null);
+        PsiField field2 = factory.createFieldFromText("boolean b = true;", null);
+        fieldList.add(field1);
+        fieldList.add(field2);
+
+        String expected =
+                "public class Temp { char c = 'c';int a = 0;boolean b = true;void doNothing() {}}";
 
         WriteCommandAction.runWriteCommandAction(project, () -> {
             AddPsi.addField(targetClass, fieldList);
