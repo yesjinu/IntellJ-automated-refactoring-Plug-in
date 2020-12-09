0ae6e0e8acc06b5549b2ca97b819797aceb596c1
kmintae <shsesther928@postech.ac.kr>
2020-12-09 21:36:16 +0900

    [S-5] [S-21] $7. feat: minor fix
    TODO: 2개 이상의 method call 호출에 대해서 확인하는 test case 추가
    TODO: IMS Statement & Expr 단위로 분해 후 조건에 따라 tempVar 생성
    TODO: IMS Debug

--- src/main/java/wanted/refactoring/InlineMethodStrengthen.java
+++ src/main/java/wanted/refactoring/InlineMethodStrengthen.java
@@ -1,11 +1,13 @@
 package wanted.refactoring;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.IndexNotReadyException;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiSearchHelper;
 import com.intellij.psi.search.searches.ClassInheritorsSearch;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.MethodSignatureUtil;
@@ -24,6 +26,12 @@ import java.util.List;
  * @author Mintae Kim
  */
 public class InlineMethodStrengthen extends InlineMethod {
+    private static int tempVarNum = 0;
+
+    @VisibleForTesting
+    public static void initVarNum() {
+        tempVarNum = 0;
+    }
 
     /* Returns the story ID. */
     @Override
@@ -43,6 +51,13 @@ public class InlineMethodStrengthen extends InlineMethod {
         // TODO: ADD
         return "<html>When a method body is more obvious than the method itself, <br/>" +
                 "Replace calls to the method with the method's content and delete the method itself.</html>";
+    }
+
+    /* Method that checks whether candidate method is refactorable. */
+    @Override
+    public boolean refactorValid(AnActionEvent e) {
+        initVarNum();
+        return super.refactorValid(e);
     }
 
     /**
@@ -75,16 +90,18 @@ public class InlineMethodStrengthen extends InlineMethod {
                 List<PsiStatement> declarations = new ArrayList<>();
 
                 // Step 1. Introduce Temporary Variable
+                introduceTemporaryVariable(methodStatement, paramList, declarations);
 
                 // Step 2. Introduce Inner Variable
+                introduceInnerVariable(methodStatement, declarations);
 
                 // Step 3. Replace Parameters (Be Aware of DummyHolder)
                 replaceElement =
                         replaceParameters(
-                                (PsiMethodCallExpression)refElement, null, replaceElement, paramList);
+                                (PsiMethodCallExpression)refElement, declarations, replaceElement, paramList);
 
                 // Step 4. Insert Statement
-                insertStatements((PsiMethodCallExpression)refElement, null, replaceElement);
+                insertStatements((PsiMethodCallExpression)refElement, declarations, replaceElement);
             }
         }
         // Delete Original Method
@@ -93,14 +110,22 @@ public class InlineMethodStrengthen extends InlineMethod {
         });
     }
 
-    private boolean introduceTemporaryVariableCondition(PsiStatement statement) {
-        // TODO: Condition
-        return true;
-    }
-    private void introduceTemporaryVariable(PsiStatement statement) {
+
+    private void introduceTemporaryVariable(PsiStatement statement,
+                                            PsiParameterList paramList, List<PsiStatement> declarations) {
         // TODO: Introduce new Statements
     }
-    private void introducteInnerVariable() {
-        // TODO: introduce new Statements
+    private void introduceInnerVariable(PsiStatement statement, List<PsiStatement> declarations) {
+        List<PsiLocalVariable> localVarList = FindPsi.findPsiLocalVariables(statement);
+
+        JavaRecursiveElementVisitor visitor = new JavaRecursiveElementVisitor() {
+            @Override
+            public void visitVariable(PsiVariable variable) {
+                super.visitVariable(variable);
+                // TODO: check equals
+            }
+        };
+
+        statement.accept(visitor);
     }
 }
