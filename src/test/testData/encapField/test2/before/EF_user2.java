33a00b35c3014c7e31a7e99171982367ad9e1e61
seha park <rachesh@postech.ac.kr>
2020-12-13 22:24:13 +0900

    refactor: replace doTestDir() to doTestFoldersMulti()
    replace method and create required test data

---
+++ src/test/testData/encapField/test2/before/EF_user2.java
@@ -0,0 +1,6 @@
+public class EF_user2 {
+    private double value;
+
+    public int method1(){ return 1; }
+
+}
---
+++ src/test/testData/encapField/test4/before/EF_owner.java
@@ -0,0 +1,10 @@
+public class EF_owner {
+    public int count;
+    private int cou<caret>nt2;
+
+    public EF_owner()
+    {
+        count = 0;
+        count2 = 0;
+    }
+}
